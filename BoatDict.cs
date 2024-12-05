fe6b246433e93e89ae21edc3bf733dcff2a047b5
OKronborg00 <oliverkronborg00@gmail.com>
2024-12-05 10:39:06 +0100

    add Boat metodes add and update

--- BoatDict.cs
+++ BoatDict.cs
@@ -8,8 +8,27 @@ namespace HillerødSialingClub
 {
     public class BoatDict
     {
-        Dictionary<Id, Boat> Boats = new Dictionary<Boat, Boat>();
+        private Dictionary<int, Boat> Boats = new Dictionary<int, Boat>();
 
+        public BoatDict() { }
 
-    }
+		public void Add(Boat boat)
+		{
+			Boats.TryAdd(boat.Id, boat);
+		}
+
+		public void Update(Boat boat)
+		{
+			if (Boats.ContainsKey(boat.Id))
+			{
+				boat.BoatType = boat.BoatType;
+				boat.Model = boat.Model;
+				boat.BoatName = boat.BoatName;
+				boat.SailingNumber = boat.SailingNumber;
+				boat.EngineInformation = boat.EngineInformation;
+				boat.Size = boat.Size;
+				boat.BuildYear = boat.BuildYear;
+			}
+		}
+	}
 }
