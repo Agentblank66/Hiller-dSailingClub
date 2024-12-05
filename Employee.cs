18d6ee990766d9c9c334e0527c18cfb9ad9ddbf0
PowerBasss447 <Kasper.sandholt@outlook.com>
2024-12-05 12:20:54 +0100

    Noter

--- Employee.cs
+++ Employee.cs
@@ -8,10 +8,17 @@ using System.Xml.Linq;
 
 namespace HillerødSialingClub
 {
+    //Her opretter vi en "Employee" som skal nedarve fra klassen "Person"
+    //Måden vi gør det på, er at gøre som illustreret nedenfor.
     public class Employee : Person
+        //For at nedarve fra klassen "Person", skal vi bruge ":".
+ 
+        //Her begynder vi på vores property.
     {
         private string Role {  get; set; }
 
+
+        //Her laver vi en constructor. 
         public Employee(string role, int id, string name, int tlf, string email, string address) : base(id, name, tlf, email, address)
         {
             Role = role;
