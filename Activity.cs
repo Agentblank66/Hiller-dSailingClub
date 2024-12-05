3cd2828fd4b7adfd336e317cc82ef027328b4466
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2024-12-05 13:48:40 +0100

    In the Class Activitity I have changed the Date type from int to DateOnly. I have changed the list from get;set; to a the list. I have added a method called JoinActivity.
    In the Class ActivityList there is added a List. There is added a AddActivity method. There is added a DeleteActivity method. There is added a GetActivity method. There is added a UpdateActivity method. There is added a SearchActivity method. There is added a ListPrint method.

--- Activity.cs
+++ Activity.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace HillerødSialingClub
@@ -11,25 +12,34 @@ namespace HillerødSialingClub
         public int Id {  get; set; } 
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Date { get; set; }
-        List<Member> Participants { get; set; }
+        public DateOnly Date { get; set; }
+
+        List<Member> Participants = new List<Member>();
 
         public Activity() { }
-        public Activity(int id, string name, string description, int date, List<Member> participants) 
+        public Activity(int id, string name, string description, DateOnly date) 
         {
             Id = id;
             Name = name;
             Description = description;
             Date = date;
-            Participants = participants;
         }
-
+        
+        //Method which overrrides the base ConsoleWriteLine
         public override string ToString()
         {
-            return $"Id: {Id} Name: {Name} Description: {Description} Date: {Date} Activity Participans: {Participants}";
+            return $"Id: {Id} Name: {Name} Description: {Description} Date: {Date}";
         }
 
-
+        //Method which adds a member to an activity, by chechking if the activity is valid and makes sure the same member can't join mulitple times. 
+        public void JoinActivity(Member member, Activity activity)
+        {
+            if (activity != null && !Participants.Contains(member))
+            {
+                Participants.Add(member);
+            }
+           
+        }
 
     }
 }
