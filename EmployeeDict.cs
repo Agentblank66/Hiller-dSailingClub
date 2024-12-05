4dbe0d3f6d9d1eb41e53b9f02a78be850502b84f
PowerBasss447 <Kasper.sandholt@outlook.com>
2024-12-05 10:18:18 +0100

    EmployeeDict

--- EmployeeDict.cs
+++ EmployeeDict.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace HillerødSialingClub
 {
-    internal class EmployeeDict
+    public class EmployeeDict
     {
+        private Dictionary<int, Employee> Employees = new Dictionary<int, Employee>();
+
+        public EmployeeDict() { }
+
+        public void Add(Employee emp)
+        {
+            Employees.TryAdd(emp.Id,emp);
+        }
+
+        public void Update(int id, Employee emp)
+        {
+            if (Employees.ContainsKey(emp.Id))
+            {
+                Employee[id]
+            }
+        }
     }
+
+      
+
+    
 }
