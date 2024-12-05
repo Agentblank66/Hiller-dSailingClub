df9c66c1f54a25cbe7ac2bd2202d0ebbda4affe3
OKronborg00 <oliverkronborg00@gmail.com>
2024-12-05 09:37:15 +0100

    Add boat class with constro

--- Boat.cs
+++ Boat.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HillerødSialingClub
+﻿namespace HillerødSialingClub
 {
-    internal class Boat
-    {
-    }
+	public class Boat
+	{
+		public int Id { get; set; }
+		public double Size { get; set; }
+		public string MotorInfo { get; set; }
+		public int BuildYear { get; set; }
+		public string Model { get; set; }
+		public string Type { get; set; }
+		public int SailNr { get; set; }
+		public string Name { get; set; }
+
+		public Boat(int id, double size, string motorInfo, int buildYear, string model, string type, int sailNr, string name)
+		{
+			Id = id;
+			Size = size;
+			MotorInfo = motorInfo;
+			BuildYear = buildYear;
+			Model = model;
+			Type = type;
+			SailNr = sailNr;
+			Name = name;
+		}
+	}
 }
