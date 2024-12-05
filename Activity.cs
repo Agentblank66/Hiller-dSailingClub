2296344b46c19ee123cf83def0f4db148fb4908c
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2024-12-05 10:02:26 +0100

    Added prorperties, constructors and ToString Method to Class Activity.

--- Activity.cs
+++ Activity.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace HillerødSialingClub
 {
-    internal class Activity
+    public class Activity
     {
+        public int Id {  get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Date { get; set; }
+        List<Member> Participants { get; set; }
+
+        public Activity() { }
+        public Activity(int id, string name, string description, int date, List<Member> participants) 
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Date = date;
+            Participants = participants;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Name: {Name} Description: {Description} Date: {Date} Activity Participans: {Participants}";
+        }
+
+
+
     }
 }
