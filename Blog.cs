c506e0b35c5a7b2bca4a0295ebbd8584a24e3f91
PowerBasss447 <Kasper.sandholt@outlook.com>
2024-12-05 11:45:19 +0100

    Blog og blog dict

--- Blog.cs
+++ Blog.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace HillerødSialingClub
 {
-    internal class Blog
+    public class Blog
     {
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public string Text { get; set; }
+
+
+        public Blog(int id, string titel, string text)
+        {
+            Id = id;
+            Titel = titel;
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} + Titel: {Titel} + Text: {Text}";
+        }
     }
 }
