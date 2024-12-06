325aac1a7bf9ad49d2c58b8e445775e947696429
OKronborg00 <oliverkronborg00@gmail.com>
2024-12-06 14:07:07 +0100

    + console test on boat class + console test on boatDict class

--- HillerødSailingClubConsoleApp/Program.cs
+++ HillerødSailingClubConsoleApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HillerødSialingClub;
+
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
-using HillerødSialingClub;
+
 
 Boat boat = new Boat(1, 4, "BodNavn", 2000, "benut", "macedes", 234, "julie");
 Activity activity = new Activity(1, "juleaften", "der holdes juleaften event", 2024, 12, 24);
@@ -16,3 +18,18 @@ BoatDict boatDict = new BoatDict();
 Console.WriteLine(boat);
 Console.WriteLine(activity);
 Console.WriteLine(blog);
+
+
+boat.AddToMaintenanceLog("testtexttoMaintencelog");
+boat.RequestRepairs("testtextToReapairlog");
+boat.PrintMaintenanceLog();
+boat.PrintRepairsLog();
+
+boatDict.Add(boat);
+boatDict.PrintAllBoat();
+boatDict.Update(1, "type", "model", "name", 123, "engineinfo", 12, 1990);
+boatDict.PrintAllBoat();
+boatDict.GetBoat(1);
+boatDict.GetBoat(2);
+boatDict.DeleteBoat(1);
+boatDict.PrintAllBoat();
