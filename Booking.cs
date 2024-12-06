bf627da3637ca4b05015b3740a46afc49fd359c4
PowerBasss447 <Kasper.sandholt@outlook.com>
2024-12-06 10:45:03 +0100

    Booking

--- Booking.cs
+++ Booking.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HillerødSialingClub
 {
-    internal class Booking
+    public class Booking
     {
+        #region Proterties
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        Member Member { get; }
+        Boat Boat { get; }
+        #endregion
+
+        #region Constructor
+        public Booking(int id, DateTime dateTime, Member member, Boat boat) 
+        { 
+            Id = id;
+            DateTime = dateTime;
+            Member = member;
+            Boat = boat;
+        }
+        #endregion
+
+
     }
 }
